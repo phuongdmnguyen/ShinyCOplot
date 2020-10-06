e9173d579eb092267960dd6c71313c34c4e5b06d
phuongdmnguyen <61225055+phuongdmnguyen@users.noreply.github.com>
2020-10-06 15:16:56 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,14 +8,13 @@ COdata <- na.omit(COdata)
 
 ## finding out names of stations where [CO] is collected from
 stations <- unique(COdata$STATION_NAME)
-# Define UI for miles per gallon application
+# Define UI 
 shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel("Plotting the CO concentration across stations in Metro Vancouver Sept-Oct 2020"),
   
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
+  # Sidebar with controls to select the stations available
   sidebarPanel(
     selectInput("station", "Station:",
                 choices = stations)
